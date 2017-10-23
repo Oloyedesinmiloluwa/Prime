4ac3eec8ff86613ad4cf8ad26efdbe228bb4f8ba
Sinmiloluwa <sinmiloluwasunday@yahoo.com>
2017-10-23 15:41:33 +0100

    Added a program that converts a prime number to string

---
+++ primetest.js
@@ -0,0 +1,18 @@
+let primeRain1=(value)=>{
+let val=prompt('Enter your number');
+  let output="";
+if(val %3==0)
+  {
+    output="Pling"
+    return output;
+  }
+ if(val %5==0 )
+  {
+    return "Plang";
+  }
+  if(val %7==0)
+  {
+    return "Plong";
+    
+  }
+}
